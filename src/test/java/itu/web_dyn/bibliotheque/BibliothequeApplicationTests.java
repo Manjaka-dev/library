7b77ebee3360f6ef7a1adcfb074c8fe906d92801
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2025-07-01 10:51:05 +0300

    init

---
+++ src/test/java/itu/web_dyn/bibliotheque/BibliothequeApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.web_dyn.bibliotheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliothequeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
