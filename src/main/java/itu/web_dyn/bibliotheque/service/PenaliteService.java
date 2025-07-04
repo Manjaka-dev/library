621e708316a8bc0669004ccc9841b60856510f72
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2025-07-04 08:58:39 +0300

    fix: modification de la contion is penalised

--- src/main/java/itu/web_dyn/bibliotheque/service/PenaliteService.java
+++ src/main/java/itu/web_dyn/bibliotheque/service/PenaliteService.java
@@ -61,7 +61,7 @@ public class PenaliteService {
         Pret pret = pretRepository.findById(idPret).orElse(null);
         Penalite penalite = null;
         if (pret != null) {
-            FinPret finPret = finPretRepository.findByPretId(pret.getIdPret()).getFirst();
+            FinPret finPret = finPretRepository.findByPretId(pret.getIdPret()).getLast();
             Retour retour = retourRepository.findByPret_IdPret(pret.getIdPret());
             if (retour !=null && finPret != null) {
                 if (retour.getDateRetour().isAfter(finPret.getDateFin())) {
